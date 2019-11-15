ca90a416cde444903e71eff9d21d8f6782d0ca89
cristhoper <cristhopermma.com>
2019-11-15 08:02:40 -0500

    F

--- ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/LoginActivity.java
+++ ChatMobile/app/src/main/java/cs2b01/utec/chat_mobile/LoginActivity.java
@@ -2,6 +2,9 @@ package cs2b01.utec.chat_mobile;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,4 +13,17 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void onBtnLoginClicked(View view){
+
+
+        EditText txtUsername = (EditText)findViewById(R.id.txtUsername);
+        EditText txtPassword = (EditText)findViewById(R.id.lblPassword);//revisar
+
+        String username = txtUsername.getText().toString();
+        String password = txtPassword.getText().toString();
+        Toast.makeText(this, username+"--"+password, Toast.LENGTH_LONG).show();
+
+    }
+
 }
